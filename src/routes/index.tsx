c45629d040a46062e011f554788c32bb20affbfb
Santiego95 <ale.santiego1@gmail.com>
2024-09-24 17:04:43 -0300

    instalando o material-ui

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@mui/material";
 import { Routes, Route, Navigate } from "react-router-dom";
 
 export const AppRoutes = () => {
     return(
         <Routes>
-            <Route path="/paginaInicial" element ={<p>Pagina Inicial</p>} />
+            <Route path="/paginaInicial" element ={<Button>Teste</Button>} />
 
 
             <Route path="*" element = {<Navigate to="paginaInicial" />} />
