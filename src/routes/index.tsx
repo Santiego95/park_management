b2a9d5c417a669d90a16114a0a27eb448fcae9c3
Santiego95 <ale.santiego1@gmail.com>
2024-09-24 16:40:52 -0300

    adicionado react-router-dom

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,14 @@
-export { };
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+    return(
+        <Routes>
+            <Route path="/paginaInicial" element ={<p>Pagina Inicial</p>} />
+
+
+            <Route path="*" element = {<Navigate to="paginaInicial" />} />
+        </Routes>
+    )
+        
+    
+};
