e21a4cb1019bd0267b37fe319bab8393afd78f5e
Nathan <nathanpaivabonifacio@gmail.com>
2024-12-10 09:09:53 -0300

    adição dos metodos de atualizar e deletar

--- src/services/cadastro-mensalista.ts
+++ src/services/cadastro-mensalista.ts
@@ -16,6 +16,19 @@ export interface CriarMensalistaResponse {
   vagas: string
 }
 
+export interface AtualizarMensalistaResponse {
+  nome?: string,
+  email?: string,
+  whatsapp?: string,
+  cpf?: string,
+  estacionamentoId?: number,
+  tipo?: string,
+  descricao?: string,
+  placa?: string,
+  vagas?: string
+}
+
+
 export interface BuscarMensalistas {
   estacionamentoId: number,
 };
@@ -46,6 +59,57 @@ export const cadastrarMensalista = async (
     return response.data;
   } catch (error: any) {
     throw new Error(error.response?.data?.message || 'Erro no cadastro de mensalista');
+  }
+};
+
+export const atualizarMensalista = async (
+  id: number,
+  nome?: string,
+  email?: string,
+  whatsapp?: string,
+  cpf?: string,
+  estacionamentoId?: number,
+  tipo?: string,
+  descricao?: string,
+  placa?: string,
+  vagas?: string
+): Promise<AtualizarMensalistaResponse> => {
+  try {
+    const response = await api.put<AtualizarMensalistaResponse>(`/mensalistas/${id}`, {
+      nome,
+      email,
+      whatsapp,
+      cpf,
+      estacionamentoId,
+      tipo,
+      descricao,
+      placa,
+      vagas,
+    });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.message || 'Erro ao atualizar mensalista');
+  }
+};
+
+export const excluirMensalista = async (
+  id: number,
+): Promise<void> => {
+  try {
+    await api.delete(`/mensalistas/${id}`);
+  } catch (error: any) {
+    throw new Error(error.response?.data?.message || 'Erro ao excluir mensalista');
+  }
+};
+
+
+
+export const buscarMensalistaPorId = async (id: number): Promise<CriarMensalistaResponse> => {
+  try {
+    const response = await api.get<CriarMensalistaResponse>(`/mensalistas/${id}`);
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response?.data?.message || 'Erro ao buscar mensalista pelo ID');
   }
 };
 
